ae07464419cdfa591ad77cda9d2cb3db17fb1d30
kelvin <kelvinvaleti@gmail.com>
2022-08-16 19:49:25 -0300

    fechamento do menu

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const btnSand = document.getElementById('btn-menu-sand');
 const menu = document.getElementById('ul-menu');
-const menuItem = document.getElementsByClassName('nav-link');
+
+const quemSomos = document.getElementById('quem-somos'); 
+const cardapio = document.getElementById('cardapio'); 
+const depoimentos = document.getElementById('depoimentos'); 
+const nossasRedes = document.getElementById('nossas-redes');
 
 let cont = false; // false se o menu NÃO estiver visível e true caso esteja
 
 //ao clicar no menu sanduiche
-btnSand.addEventListener("click", function(){    
+btnSand.addEventListener("click", function(){ 
     if(cont==false){
         menu.style.display ='block';
         cont = true;
@@ -15,13 +19,19 @@ btnSand.addEventListener("click", function(){
     }
 });
 
-/*
+menu.addEventListener("click", fechaMenu);
+quemSomos.addEventListener("click", fechaMenu);
+cardapio.addEventListener("click", fechaMenu);
+depoimentos.addEventListener("click", fechaMenu);
+nossasRedes.addEventListener("click", fechaMenu);
+
+
 function fechaMenu(){
-    if(cont==true){
+    if(cont == true){
         menu.style.display ='none';
         cont = false;  
     }
-}*/
+}
 
 
 
