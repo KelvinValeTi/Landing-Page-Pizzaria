4c62f6711e36894133e836c7b4644af1202c9e26
kelvin <kelvinvaleti@gmail.com>
2022-11-21 16:51:53 -0300

    ajustes responsividade

--- script.js
+++ script.js
@@ -20,12 +20,27 @@ btnSand.addEventListener("click", function(){
     }
 });
 
+//caso o tela esteja com no minimo 791px de largura, o menu volta a aparecer. 
+const mediaQuery = window.matchMedia('(min-width: 791px)');
+
+function handleTabletChange(e) {
+  //checa se a tela está no tamanho minimo de 791px
+  if (e.matches) {
+    menu.style.display='flex';
+    menu.style.flexDirection='row';
+    menu.style.justifyContent='flex-end';
+    menu.style.alignItems='center';
+  }else{
+    menu.style.display='none';
+  }
+}
+
+mediaQuery.addListener(handleTabletChange);
+
+handleTabletChange(mediaQuery);
+
 //Se o menu estiver aberto, ele fecha caso algum elemento fora do menu seja clicado.
 menu.addEventListener("click", fechaMenu);
-quemSomos.addEventListener("click", fechaMenu);
-cardapio.addEventListener("click", fechaMenu);
-depoimentos.addEventListener("click", fechaMenu);
-nossasRedes.addEventListener("click", fechaMenu);
 
 //fecha o menu, caso esteja aberto.
 function fechaMenu(){
